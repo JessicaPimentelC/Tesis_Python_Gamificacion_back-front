ebfa1eb1e2b9688b9c01b6774559b889e011f1f3
JessicaPimentelC <jessia.pimentel@correounivalle.edu.co>
2025-05-10 23:56:43 -0500

    permisos usuario admin

--- frontend/src/utils/validacionUsuario.js
+++ frontend/src/utils/validacionUsuario.js
@@ -2,7 +2,7 @@ const adminEmails = [
   "admin@gmail.com",
   "usuario@gmail.com",
   "jessica.pimentel@correounivalle.edu.co",
-  "eduardo.daza@correounivalle.edu.co.co"
+  "eduardo.daza@correounivalle.edu.co"
 ];
 export const esAdmin = (userInfo) => {
   return userInfo && adminEmails.includes(userInfo.email);
