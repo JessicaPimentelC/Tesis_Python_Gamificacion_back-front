ebf54ee3419b58e1d7227f8dfa8486443a36e628
JessicaPimentelC <jessia.pimentel@correounivalle.edu.co>
2025-05-11 20:50:13 -0500

    ajuste token user

--- frontend/src/utils/userService.js
+++ frontend/src/utils/userService.js
@@ -1,9 +1,9 @@
+import { useNavigate } from 'react-router-dom'; // Importar navigate desde react-router-dom
 import axios from 'axios';
-import { getCSRFToken, refreshAccessToken, navigate } from '../utils/validacionesGenerales';
+import { getCSRFToken, refreshAccessToken } from '../utils/validacionesGenerales';
 import API_BASE_URL from '../config';
 
 export const fetchUserInfo = async () => {
-    
   try {
     const csrfToken = getCSRFToken();
     const config = {
@@ -25,20 +25,25 @@ export const fetchUserInfo = async () => {
 
   } catch (error) {
     console.error('Error fetching user:', error);
-    
+
     // Manejo de token expirado (opcional)
     if (error.response?.status === 401) {
       try {
         const newToken = await refreshAccessToken();
         localStorage.setItem('access_token', newToken);
-        return await fetchUserInfo(); 
+
+        // Intentar hacer la solicitud de nuevo con el nuevo token
+        const retryResponse = await fetchUserInfo(); 
+        return retryResponse; 
       } catch (refreshError) {
+        // Si no se puede refrescar el token, eliminar los tokens y redirigir al login
         localStorage.removeItem('access_token');
-        window.location.href = '/'; 
+        localStorage.removeItem('refresh_token');
+        window.location.href = '/';  // Redirección usando window.location
         throw refreshError;
       }
     }
-    
+
     throw error; // Propagar el error para manejo específico
   }
 };
