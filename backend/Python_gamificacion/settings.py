53ad0e52590705827c0d9b1989dfda08cb0c4bf3
JessicaPimentelC <jessia.pimentel@correounivalle.edu.co>
2025-06-13 01:35:13 -0500

    se elimina blacklist

--- backend/Python_gamificacion/settings.py
+++ backend/Python_gamificacion/settings.py
@@ -201,5 +201,5 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=40),  
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),    
     'ROTATE_REFRESH_TOKENS': True,                  
-    'BLACKLIST_AFTER_ROTATION': True                
+    'BLACKLIST_AFTER_ROTATION': False                
 }
